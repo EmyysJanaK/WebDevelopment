41a79a7d606ccf5b0af247a9d56350e4b850a87e
janak <janaksenevirathne8@gmail.com>
2023-06-01 23:07:03 +0530

    Update script.js

--- cont1/script.js
+++ cont1/script.js
@@ -1,15 +1,38 @@
-// Log a message to the console when the form is submitted
+// Require the nodemailer module
+const nodemailer = require('nodemailer');
+
+// Create a transporter using Gmail SMTP
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'your-email@gmail.com',
+    pass: 'your-password'
+  }
+});
+
+// Handle form submission
 document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting
-  
-    // Get form values
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
-  
-    // Log the values to the console
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Message:', message);
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Get form values
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+
+  // Prepare the email message
+  var mailOptions = {
+    from: 'your-email@gmail.com',
+    to: 'recipient-email@example.com',
+    subject: 'New Message from Your Web Page',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log('Error:', error);
+    } else {
+      console.log('Email sent:', info.response);
+    }
   });
-  
+});
