7bfafd8eb420d15626801ba1ad78be7b242a3df5
JanakCSE210598B <janaksenevirathne8@gmail.com>
2023-07-26 23:58:20 +0530

    updated

---
+++ onlinestore/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const popupTrigger = document.querySelector('.popup-trigger');
+    const popup = document.querySelector('.popup');
+
+    popupTrigger.addEventListener('click', function () {
+        popup.style.display = 'block'; // Show the pop-up
+        setTimeout(function () {
+            popup.style.display = 'none'; // Hide the pop-up after a certain time (optional)
+        }, 3000); // Adjust the time (in milliseconds) as per your requirement
+    });
+});
