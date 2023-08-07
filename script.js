1983782e92313ff124f4fb3546eb845a000977a1
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-08-07 21:45:50 +0530

    SOLVED

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ function toggleTheme() {
   container.classList.toggle('dark-theme');
 }
 
+// Function to handle theme change animation
+function handleThemeChange() {
+  // Check if the dark theme is active
+  if (container.classList.contains('dark-theme')) {
+    container.style.backgroundColor = '#333'; // Dark background
+    container.style.color = '#fff'; // Light text
+  } else {
+    container.style.backgroundColor = '#fff'; // Light background
+    container.style.color = '#000'; // Dark text
+  }
+}
 // Add animation class and remove after animation ends
 function animateThemeChange() {
   container.classList.add('animating');
