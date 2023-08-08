97512097fe5b5ec3b4a0bed0c148116191428521
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-08-08 21:45:50 +0530

    SOLVED

--- script1.js
+++ script1.js
@@ -4,3 +4,13 @@ const button = document.getElementById('theme-button');
 button.addEventListener('click', () => {
   container.classList.toggle('dark-theme');
 });
+
+container.addEventListener('transitionend', () => {
+  if (container.classList.contains('dark-theme')) {
+    container.style.backgroundColor = '#333'; // Dark background
+    container.style.color = '#fff'; // Light text
+  } else {
+    container.style.backgroundColor = '#fff'; // Light background
+    container.style.color = '#000'; // Dark text
+  }
+});
