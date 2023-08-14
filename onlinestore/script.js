a7abb3d0aef323308ea935abaff0345ffc4f7077
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-08-14 21:45:50 +0530

    SOLVED

--- onlinestore/script.js
+++ onlinestore/script.js
@@ -8,5 +8,29 @@ document.addEventListener('DOMContentLoaded', function () {
             popup.style.display = 'none'; // Hide the pop-up after a certain time (optional)
         }, 3000); // Adjust the time (in milliseconds) as per your requirement
     });
+    const copyButtons = document.querySelectorAll('.copy-btn'); 
+    copyButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const codeElement = button.previousElementSibling;
+            const range = document.createRange();
+            range.selectNodeContents(codeElement);
+            const selection = window.getSelection();
+            selection.removeAllRanges();
+            selection.addRange(range);
+            
+            // Copy the selected text to the clipboard
+            document.execCommand('copy');
+            console.log('Code copied to clipboard:', codeElement.innerText);
+            
+            // Remove the selection
+            selection.removeAllRanges();
+            
+            // Change button text to 'Copied!'
+            button.innerText = 'Copied!';
+            setTimeout(() => {
+                button.innerText = 'Copy';
+            }, 1500); // Reset the button text after 1.5 seconds
+        });
+    });
 });
 huytth
